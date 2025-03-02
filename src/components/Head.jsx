0cb150bee61404e584af2571266d64429aef60e7
Raunak <raunakkumar7245@gmail.com>
2025-03-02 16:40:10 +0530

    Initial commit

---
+++ src/components/Head.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  RiMenuLine,
+  RiSearchLine,
+  RiUserLine,
+  RiYoutubeFill,
+} from "@remixicon/react";
+import { toggleSidebar } from "../utils/sidebarSlice";
+import { useDispatch } from "react-redux";
+const Head = () => {
+  const dispatch = useDispatch();
+  return (
+    <div className="flex justify-between items-center py-2 px-4 scale-y-95 md:scale-y-100">
+      <div className="flex gap-3 items-center">
+        <button
+          onClick={() => {
+            dispatch(toggleSidebar());
+          }}
+        >
+          <RiMenuLine className="text-gray-600 h-[42px] w-[42px] p-[10px] hover:bg-gray-100 rounded-full" />
+        </button>
+
+        <RiYoutubeFill size={30} className="text-red-500" />
+      </div>
+      <div className="flex items-center border-1 border-gray-300 bg-gray-50 hover:bg-gray-100 rounded-full">
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-[38vw] h-10 bg-white rounded-l-full border-r-1 border-gray-300 px-4 focus:outline-1 outline-blue-500"
+        />
+        <button className="h-full px-4">
+          <RiSearchLine className="text-gray-500" />
+        </button>
+      </div>
+      <div className="flex">
+        <RiUserLine className="text-gray-600 h-[42px] w-[42px] p-[10px] hover:bg-gray-100 rounded-full" />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,56 @@
+import {
+  RiFireLine,
+  RiHome4Line,
+  RiLogoutBoxLine,
+  RiMenuUnfoldFill,
+  RiQuestionLine,
+  RiSettings3Line,
+  RiTimeLine,
+  RiYoutubeLine,
+} from "@remixicon/react";
+import React from "react";
+import { useSelector } from "react-redux";
+const Sidebar = () => {
+  const showSlidebar = useSelector((store) => store.slidebar.isOpen);
+  if (!showSlidebar) return <div></div>;
+  return (
+    <div className="border-r border-gray-100 h-[calc(100vh-4rem)] absolute md:static z-10 bg-white">
+      <ul className="p-4 font-semibold text-gray-700 w-52">
+        <li className="p-2 m-1 hover:bg-gray-100 rounded-lg flex gap-1">
+          <RiHome4Line />
+          Home
+        </li>
+        <li className="p-2 m-1 hover:bg-gray-100 rounded-lg flex gap-1">
+          <RiFireLine />
+          Trending
+        </li>
+        <li className="p-2 m-1 hover:bg-gray-100 rounded-lg flex gap-1">
+          <RiYoutubeLine />
+          Subscriptions
+        </li>
+        <li className="p-2 m-1 hover:bg-gray-100 rounded-lg flex gap-1">
+          <RiMenuUnfoldFill />
+          Playlist
+        </li>
+        <li className="p-2 m-1 hover:bg-gray-100 rounded-lg flex gap-1">
+          <RiTimeLine />
+          History
+        </li>
+        <li className="p-2 m-1 hover:bg-gray-100 rounded-lg flex gap-1">
+          <RiSettings3Line />
+          Settings
+        </li>
+        <li className="p-2 m-1 hover:bg-gray-100 rounded-lg flex gap-1">
+          <RiQuestionLine />
+          Help
+        </li>
+        <li className="p-2 m-1 hover:bg-gray-100 rounded-lg flex gap-1">
+          <RiLogoutBoxLine />
+          Log Out
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
